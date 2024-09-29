30b69e8bbe08881fa9806e983991b5d611bca392
Angus Bower-Brown <angus.bower-brown@dentally.co>
2024-09-29 22:16:40 +0100

    autocomplete

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,24 +9,31 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls" },
-        auto_install = true,
-      })
-    end,
+    opts = {
+      auto_install = true,
+    },
   },
   {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.ts_ls.setup({})
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities
+      })
+
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
     end,
   },
 }
