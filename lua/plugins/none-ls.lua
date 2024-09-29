02bf92b4385d92ed4449b9e2e1aab0ae1afce442
Angus Bower-Brown <angus.bower-brown@dentally.co>
2024-09-29 18:42:02 +0100

    simplify setup for builtin conditional

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,31 +2,26 @@ return {
   "nvimtools/none-ls.nvim",
   config = function()
     local null_ls = require("null-ls")
-    local conditional = function(fn)
+    local conditional = function(builtin)
       local utils = require("null-ls.utils").make_conditional_utils()
-      return fn(utils)
+      if utils.root_has_file("Gemfile") then
+        return builtin.with({
+          command = "bundle",
+          args = vim.list_extend({ "exec", "rubocop" }, builtin._opts.args),
+        })
+      else
+        return builtin
+      end
     end
-   null_ls.setup({
+
+    null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        conditional(function(utils)
-          return utils.root_has_file("Gemfile")
-                  and null_ls.builtins.diagnostics.rubocop.with({
-                      command = "bundle",
-                      args = vim.list_extend({ "exec", "rubocop" }, null_ls.builtins.diagnostics.rubocop._opts.args),
-                  })
-              or null_ls.builtins.diagnostics.rubocop
-        end),
-        conditional(function(utils)
-          return utils.root_has_file("Gemfile")
-                  and null_ls.builtins.formatting.rubocop.with({
-                      command = "bundle",
-                      args = vim.list_extend({ "exec", "rubocop" }, null_ls.builtins.formatting.rubocop._opts.args),
-                  })
-              or null_ls.builtins.formatting.rubocop
-        end),
+        conditional(null_ls.builtins.diagnostics.rubocop),
+        conditional(null_ls.builtins.formatting.rubocop),
       },
     })
+
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
   end,
 }
