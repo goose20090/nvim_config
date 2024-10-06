040d967236664ef1d0c5728d3aa6e1b4fa85710b
Angus Bower-Brown <angus.bower-brown@dentally.co>
2024-10-06 13:22:38 +0100

    lsp config tweask

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,23 +17,32 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       local lspconfig = require("lspconfig")
       lspconfig.ts_ls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.ruby_lsp.setup({
+        init_options = {
+          formatter = "standard",
+          linters = { "standard" },
+        },
+        capabilities = capabilities,
+        cmd = { vim.fn.expand("~/.asdf/shims/ruby-lsp") },
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
       vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
     end,
   },
 }
