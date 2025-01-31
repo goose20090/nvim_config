ee02bfb9908eb4574edf412ce49a7302effedb06
Angus Bower-Brown <angus.bower-brown@dentally.co>
2025-01-31 14:19:03 +0000

    stashing changes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,54 +5,7 @@ return {
     config = function()
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '//', builtin.find_files, {})
-      vim.keymap.set('n', '<space>g', builtin.live_grep, {})
-
-      -- Harpoon setup
-      local harpoon = require('harpoon')
-      harpoon:setup({})
-
-      -- Enhanced toggle_telescope function with file removal
-      local function toggle_telescope(harpoon_files)
-          local finder = function()
-              local paths = {}
-              for _, item in ipairs(harpoon_files.items) do
-                  table.insert(paths, item.value)
-              end
-
-              return require("telescope.finders").new_table({
-                  results = paths,
-              })
-          end
-
-          local conf = require("telescope.config").values
-          require("telescope.pickers").new({}, {
-              prompt_title = "Harpoon",
-              finder = finder(),
-              previewer = conf.file_previewer({}),
-              sorter = require("telescope.config").values.generic_sorter({}),
-              layout_config = {
-                  height = 0.4,
-                  width = 0.5,
-                  prompt_position = "top",
-                  preview_cutoff = 120,
-              },
-              attach_mappings = function(prompt_bufnr, map)
-                  map("i", "<C-d>", function()
-                      local state = require("telescope.actions.state")
-                      local selected_entry = state.get_selected_entry()
-                      local current_picker = state.get_current_picker(prompt_bufnr)
-
-                      -- Remove the selected item from Harpoon
-                      table.remove(harpoon_files.items, selected_entry.index)
-                      current_picker:refresh(finder())
-                  end)
-                  return true
-              end,
-          }):find()
-      end
-
-      -- Key mapping for opening Harpoon with Telescope
-      vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
+      vim.keymap.set('n', '<leader>gg', builtin.live_grep, {})
     end
   },
   {
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,6 +5,11 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set number relativenumber")
 vim.g.mapleader= " "
 vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
-vim.cmd([[autocmd FileType ruby setlocal indentkeys-=.]])
+vim.cmd([[autocmd FileType ruby setlocal indentkeys-=]])
 
+
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
